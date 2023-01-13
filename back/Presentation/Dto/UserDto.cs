3b6c57da0b08af0f299718bd5926ac3594d68273
Focus <maniaclord@bk.ru>
2023-01-13 12:04:48 +0300

    prolly working auth controller xd

---
+++ back/Presentation/Dto/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.Dto;
+
+public class UserDto
+{
+    [Required] 
+    public required string Username { get; set; }
+
+    [Required] 
+    public required string Password { get; set; }
+}
