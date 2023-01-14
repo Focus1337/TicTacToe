a6443a52a1073736d88d4e5752e318e7cf6e3a91
mansur <ahmatowmansur@gmail.com>
2023-01-14 10:34:44 +0300

    fweoigoijwe

--- back/EventBusHandler/Program.cs
+++ back/EventBusHandler/Program.cs
@@ -5,29 +5,37 @@ using EventBusHandler.RabbitMq;
 using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Client;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var services = builder.Services;
-
-services
-    .Configure<DbOptions>(
-        builder.Configuration.GetSection(DbOptions.DbConfiguration))
-    .Configure<RabbitOptions>(
-        builder.Configuration.GetSection(RabbitOptions.RabbitConfiguration));
-
-var rabbitOptions = builder.Configuration.GetSection(RabbitOptions.RabbitConfiguration).Get<RabbitOptions>();
-var dbOptions = builder.Configuration.GetSection(DbOptions.DbConfiguration).Get<DbOptions>();
-
-services.AddSingleton(new ConnectionFactory
+start:
+try
 {
-    HostName = rabbitOptions!.HostName,
-    DispatchConsumersAsync = true
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-services.AddSingleton<GameUpdateConsumer>();
-services.AddHostedService<GameUpdateBusHandler>();
-services.AddDbContext<AppDbContext>(options => options.UseNpgsql(dbOptions!.ConnectionString));
+    var services = builder.Services;
 
-var app = builder.Build();
+    services
+        .Configure<DbOptions>(
+            builder.Configuration.GetSection(DbOptions.DbConfiguration))
+        .Configure<RabbitOptions>(
+            builder.Configuration.GetSection(RabbitOptions.RabbitConfiguration));
 
-app.Run();
+    var rabbitOptions = builder.Configuration.GetSection(RabbitOptions.RabbitConfiguration).Get<RabbitOptions>();
+    var dbOptions = builder.Configuration.GetSection(DbOptions.DbConfiguration).Get<DbOptions>();
+
+    services.AddSingleton(new ConnectionFactory
+    {
+        HostName = rabbitOptions!.HostName,
+        DispatchConsumersAsync = true
+    });
+
+    services.AddSingleton<GameUpdateConsumer>();
+    services.AddHostedService<GameUpdateBusHandler>();
+    services.AddDbContext<AppDbContext>(options => options.UseNpgsql(dbOptions!.ConnectionString));
+
+    var app = builder.Build();
+
+    app.Run();
+}
+catch
+{
+    goto start;
+}
